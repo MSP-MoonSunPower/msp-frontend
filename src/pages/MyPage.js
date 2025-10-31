7f80f91e3788a341b57d9f94567d2b7311d0a6af
xxj15 <seojini00@naver.com>
2025-10-31 15:28:10 +0900

    chore: mypage, login 탭 삭제

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -2,17 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./MyPage.module.css";
 
-function MyPage() {
-  const navigate = useNavigate();
-  const [alertShown, setAlertShown] = useState(false);
-
-  useEffect(() => {
-    if (!alertShown) {
-      setAlertShown(true);
-      alert("MyPage는 12월 중에 만나볼 수 있습니다!");
-      navigate("/");
-    }
-  }, [alertShown, navigate]);
-}
+function MyPage() {}
 
 export default MyPage;
